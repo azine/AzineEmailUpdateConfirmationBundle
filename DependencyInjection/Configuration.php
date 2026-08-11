13c7b9314f189a15bbd0273249c8a3563c67940d
azine.me <github@azine-it.ch>
2026-08-11 22:16:50 +0200

    Modernize email update configuration for Symfony 7.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Azine\EmailUpdateConfirmationBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
@@ -7,21 +9,35 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('azine_email_update_confirmation');
-        $rootNode = $treeBuilder->getRootNode();
 
-        $rootNode->children()
-                ->booleanNode('enabled')->defaultTrue()->info('enable/disable email update confirmation functionality. default = true')->end()
-                ->scalarNode('cypher_method')->defaultNull()->info('determines the encryption mode for encryption of email value. openssl_get_cipher_methods(false) is default value')->end()
-                ->scalarNode('mailer')->defaultValue('azine.email_update.mailer')->info('mailer service to be used')->end()
-                ->scalarNode('email_template')->defaultValue('@AzineEmailUpdateConfirmation/Email/email_update_confirmation.txt.twig')->info('email template')->end()
-                ->scalarNode('from_email')->defaultNull()->info('`from`-address for the email. If not set, `fos_user.resetting.email.from_email` will be used')->end()
-                ->scalarNode('redirect_route')->defaultValue('fos_user_profile_show')->info('route to redirect to, after the update confirmation')->end()
+        $treeBuilder->getRootNode()
+            ->children()
+                ->booleanNode('enabled')
+                    ->defaultTrue()
+                ->end()
+                ->scalarNode('cypher_method')
+                    ->defaultNull()
+                ->end()
+                ->scalarNode('mailer')
+                    ->defaultValue('azine.email_update.default_mailer')
+                ->end()
+                ->scalarNode('email_template')
+                    ->defaultValue('@AzineEmailUpdateConfirmation/Email/email_update_confirmation.txt.twig')
+                ->end()
+                ->variableNode('from_email')
+                    ->defaultNull()
+                    ->validate()
+                        ->ifTrue(static fn (mixed $value): bool => null !== $value && !is_string($value) && !is_array($value))
+                        ->thenInvalid('Expected from_email to be a string or an address/name array.')
+                    ->end()
+                ->end()
+                ->scalarNode('redirect_route')
+                    ->cannotBeEmpty()
+                    ->defaultValue('fos_user_profile_show')
+                ->end()
             ->end();
 
         return $treeBuilder;
